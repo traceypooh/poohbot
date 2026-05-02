029df5d76822eeede505814ed191d433ac24cf82
Tracey Jaquith <tracey@archive.org>
2026-05-02 14:26:07 -0700

    convert to ESM; dump jquery

--- lacer/lacer.js
+++ lacer/lacer.js
@@ -3,6 +3,8 @@
 // Example:
 //     bash lacer.txt  good.mov  0.5  0.7
 
+/* eslint-disable no-console, no-nested-ternary */
+
 const BFF = true // DV is typically Bottom Field First.  Set to 0/falsey if Top Field First
 const BASE = 'https://archive.org/download/lacer/' // imagery base url
 
@@ -14,26 +16,25 @@ const HT = 240
 function arg(theArgName) {
   const sArgs = location.search.slice(1).split('&')
   let r = ''
-  for (var i = 0; i < sArgs.length; i++) {
-    if (sArgs[i].slice(0,sArgs[i].indexOf('=')) === theArgName) {
+  for (let i = 0; i < sArgs.length; i++) {
+    if (sArgs[i].slice(0, sArgs[i].indexOf('=')) === theArgName) {
       r = sArgs[i].slice(sArgs[i].indexOf('=') + 1)
       break
     }
   }
-  return (r.length > 0 ? unescape(r).split(',') : '')
+  return r.length > 0 ? unescape(r).split(',') : ''
 }
 
 
-$.getJSON('https://archive.org/metadata/lacer/files', (json) => {
-
+fetch('https://archive.org/metadata/lacer/files').then((r) => r.json()).then((json) => {
   console.log(json)
 
   let IMGS = []
   const twothree = arg('alt')  ||  arg('deinterlaced')
   for (const file of json.result) {
     const twothreeFi = file.name.match(/2323\/.*\.png$/)
-    if (( twothree  &&   twothreeFi)  ||
-        (!twothree  &&  !twothreeFi  &&  file.name.match(/[^\/]+\.png$/))) {
+    if ((twothree  &&   twothreeFi)  ||
+        (!twothree  &&  !twothreeFi  &&  file.name.match(/[^/]+\.png$/))) {
       console.log(file.name)
       IMGS.push(file.name)
     }
@@ -44,22 +45,22 @@ $.getJSON('https://archive.org/metadata/lacer/files', (json) => {
   if (arg('deinterlaced')) {
     // counting first field as 0, throw out fields 3 and 4.
     // continue that pattern every 10 fields...
-    IMGS = IMGS.filter((val,idx) => (3 !== idx % 10  &&  4 !== idx % 10))
+    IMGS = IMGS.filter((val, idx) => (idx % 10 !== 3  &&  idx % 10 !== 4))
   }
 
 
   let htm = ''
   if (arg('all')) {
-    for (let im of IMGS)
+    for (const im of IMGS)
       htm += `<div class="ttl ghost roundbox5">${im}</div><img src="${BASE + im}"><br/>`
   }
 
 
-  const shift = (arg('shift') === '' ? 0 : parseInt(arg('shift'), 0))
+  const shift = (arg('shift') === '' ? 0 : parseInt(arg('shift'), 10))
   const url =
-    '/lacer/?' +
-    (arg('alt') ? 'alt=1&' : '') +
-    (arg('deinterlaced') ? 'deinterlaced=1&' : '')
+    `/lacer/?${
+      arg('alt') ? 'alt=1&' : ''
+    }${arg('deinterlaced') ? 'deinterlaced=1&' : ''}`
 
 
   if (arg('pick') !== '') {
@@ -86,7 +87,7 @@ $.getJSON('https://archive.org/metadata/lacer/files', (json) => {
   <br/>
   <a href="${url}all=1">show all fields</a>`
 
-    $('#js-form').html(htm)
+    document.getElementById('js-form').innerHTML = htm
     return
   }
 
@@ -98,48 +99,46 @@ $.getJSON('https://archive.org/metadata/lacer/files', (json) => {
   const b = BASE + IMGS[bN]
 
 
-
-  htm += `<div id="lacer" style="width:${WD}px; height:${2*HT}px">`
+  htm += `<div id="lacer" style="width:${WD}px; height:${2 * HT}px">`
   for (let y = 0; y <= HT; y++) {
     // CSS MAGIC!
-    const line = `clip:rect(${y}px ${WD}px ${y+1}px 0px)`
-    htm += `<img src="${BFF ? b : t}" style="top:${y  }px; ${line};">`
-    htm += `<img src="${BFF ? t : b}" style="top:${y+1}px; ${line}; left:${shift}px;">`
+    const line = `clip:rect(${y}px ${WD}px ${y + 1}px 0px)`
+    htm += `<img src="${BFF ? b : t}" style="top:${y}px; ${line};">`
+    htm += `<img src="${BFF ? t : b}" style="top:${y + 1}px; ${line}; left:${shift}px;">`
   }
   htm += '</div>'
 
-  htm += (BFF
-? `bot ==> ${b}<br/>top ==> ${t}<br/>
+  htm += `${BFF
+    ? `bot ==> ${b}<br/>top ==> ${t}<br/>
 <div class="note">
   (NOTE: showing bottom field first since DV source,
   so first line you see above is from a 'bottom field')
 </div><br/>`
-: `top ==> ${t}<br/>bot ==> ${b}<br/>`
-) + `
- [ <a href="${url}bN=${bN+2}&tN=${tN+2}">NEXT PAIR</a>
- | <a href="${url}bN=${bN-2}&tN=${tN-2}">PREV PAIR</a>
+    : `top ==> ${t}<br/>bot ==> ${b}<br/>`
+  }
+ [ <a href="${url}bN=${bN + 2}&tN=${tN + 2}">NEXT PAIR</a>
+ | <a href="${url}bN=${bN - 2}&tN=${tN - 2}">PREV PAIR</a>
  | <a href="${url}bN=${tN}&tN=${bN}">SWAP TOP/BOTTOM</a>
- | <a href="${url}bN=${bN}&tN=${tN}&shift=${shift-1}">SHIFT X LEFT 1</a>
- | <a href="${url}bN=${bN}&tN=${tN}&shift=${shift+1}">SHIFT X RIGHT 1</a>
+ | <a href="${url}bN=${bN}&tN=${tN}&shift=${shift - 1}">SHIFT X LEFT 1</a>
+ | <a href="${url}bN=${bN}&tN=${tN}&shift=${shift + 1}">SHIFT X RIGHT 1</a>
  | <a href="/lacer/">START OVER</a>
  | <a href="${url}pick=1">PICK TWO FIELDS</a>
- ]<br/>` +
+ ]<br/>${
 
-(arg('alt')
-? ` [ <a href="/lacer/">original set</a>
+  arg('alt')
+    ? ` [ <a href="/lacer/">original set</a>
  | alternate 3:2 pulldown DV sample set
  | <a href="/lacer/?deinterlaced=1">alternate set - deinterlaced</a>
  ] <br>`
-: (arg('deinterlaced')
-  ? `[ <a href="/lacer/">original set</a>
+    : (arg('deinterlaced')
+      ? `[ <a href="/lacer/">original set</a>
    | <a href="/lacer/?alt=1">alternate 3:2 pulldown DV sample set</a>
    | alternate set - deinterlaced
    ] <br>`
-  : `[ original set".
+      : `[ original set".
    | <a href="/lacer/?alt=1">alternate 3:2 pulldown DV sample set</a>
    | <a href="/lacer/?deinterlaced=1">alternate set - deinterlaced</a>
    ] <br>`
-  )
-)
-  $('#js-form').html(htm)
+    )}`
+  document.getElementById('js-form').innerHTML = htm
 })
